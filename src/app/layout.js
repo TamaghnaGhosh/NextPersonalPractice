064ad53dbccabdfad46ae1e56171c2281bd64abc
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-04-18 21:15:16 +0530

    which is added in the public folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/no-css-tags */
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,12 +14,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
+        <link rel="stylesheet" href="/style.css" />
+        <script src="/common.js"></script>
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
