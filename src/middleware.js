590f9709512c958020b99267555bf117b23de36e
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-04-18 19:54:59 +0530

    !pathname.startsWith("/about/")

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+  const { pathname } = request.nextUrl;
   //   if (request.nextUrl.pathname != "/login") {
   //   return NextResponse.redirect(new URL("/login", request.url));
   //   }
 
-  return NextResponse.redirect(new URL("/login", request.url));
+  // return NextResponse.redirect(new URL("/login", request.url));
+  if (pathname !== "/login" && !pathname.startsWith("/about/")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
 
 // Can't go inside these url as it always redirects to login page..
