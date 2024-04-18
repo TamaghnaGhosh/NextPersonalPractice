fc1840d184f462c3b61f1f2e8f67ebeb8ff2b247
TamaghnaGhosh <ghoshtamaghna6991@gmail.com>
2024-04-18 20:59:23 +0530

    loading added for server componenet data fetching

---
+++ src/app/serverapiproductlist/loading.js
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="items-center text-center">
+      <h1 className="font-semibold text-4xl">Loading Product list.......</h1>
+    </div>
+  );
+}
